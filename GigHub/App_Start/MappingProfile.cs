c1c292657a5d68f686c27b1027289836eb95961a
antcalatayud <ant.calatayud@gmail.com>
2017-11-10 16:32:55 +0000

    Return Notification with API. Chane to DTOs and install automapper.

---
+++ GigHub/App_Start/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+
+namespace GigHub.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public IMapper mapper { get; set; }
+
+        public MappingProfile()
+        {
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ApplicationUser, UserDto>();
+                cfg.CreateMap<Genre, GenreDto>();
+                cfg.CreateMap<Gig, GigDto>();
+                cfg.CreateMap<Notification, NotificationDto>();
+            });
+
+            mapper = config.CreateMapper();
+
+        }
+    }
+}
