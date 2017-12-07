ae2a22aa4d12f87ce85161a55275adb72dae3a4b
antcalatayud <ant.calatayud@gmail.com>
2017-12-07 16:45:41 +0000

    Start refactoring JS

---
+++ GigHub/Scripts/app/app.js
@@ -0,0 +1,44 @@
+﻿var GigsController = function () {
+
+    var button;
+
+    var init = function () {
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };    
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+        if (button.hasClass("btn-default"))
+            createAttendance();
+        else
+            deleteAttendance();
+    };
+
+    var fail = function () {
+        alert("Something failed");
+    };
+
+    var done = function () {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var createAttendance = function () {
+        $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+           .done(done)
+           .fail(fail);
+    };
+
+    var deleteAttendance = function () {
+        $.ajax({
+            url: "/api/attendances/" + button.attr("data-gig-id"),
+            method: "DELETE"
+        })
+             .done(done)
+             .fail(fail);
+    };
+
+    return {
+        init: init
+    }
+}();
