4cc05f7ec1786713205adc4b3861d9bfc69c9edf
antcalatayud <ant.calatayud@gmail.com>
2017-12-11 14:47:36 +0000

    Create Unit Of Work

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using GigHub.Persistence;
 using GigHub.Repositories;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
@@ -14,6 +15,7 @@ namespace GigHub.Controllers
         private readonly AttendanceRepository _attendanceRepository;
         private readonly FollowRepository _followRepository;
         private readonly GenreRepository _genreRepository;
+        private readonly UnitOfWork _unitOfWork;
 
         public GigsController()
         {
@@ -22,6 +24,7 @@ namespace GigHub.Controllers
             _attendanceRepository = new AttendanceRepository(_context);
             _followRepository = new FollowRepository(_context);
             _genreRepository = new GenreRepository(_context);
+            _unitOfWork = new UnitOfWork(_context);
         }
 
         [Authorize]
@@ -83,9 +86,9 @@ namespace GigHub.Controllers
                 Venue = viewModel.Venue
             };
 
-            _gigRepository.Add(gig); 
+            _gigRepository.Add(gig);
 
-            _context.SaveChanges();
+            _unitOfWork.Complete();
 
             return RedirectToAction("Mine", "Gigs");
         }
@@ -137,7 +140,7 @@ namespace GigHub.Controllers
 
             gig.Modify(viewModel.GetDateTime(), viewModel.Venue, viewModel.Genre);
 
-            _context.SaveChanges();
+            _unitOfWork.Complete();
 
             return RedirectToAction("Mine", "Gigs");
         }
